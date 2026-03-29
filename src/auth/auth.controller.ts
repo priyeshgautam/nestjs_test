0b2b23de5a4f2881b2ff239db5d98b067fac8117
priyesh <priyeshgautam8@gmail.com>
2026-03-29 21:25:31 +0530

    call userService from authService

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AuthService } from './auth.service'
+import { UserService } from '../user/user.service';
 
 @Controller('auth')
 export class AuthController {
-    authService: AuthService;
 
-  constructor(authService: AuthService) {
-    this.authService = authService;
-  }
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
   @Get('getHello')
   getHello(){
     return this.authService.getHello();
+  }
+
+  registerUser(){
+    this.userService.createUser()
   }
 }
