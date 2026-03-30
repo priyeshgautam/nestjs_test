98d57e61de50ee946704628edbd90a20fbe3b4fe
priyesh <priyeshgautam8@gmail.com>
2026-03-30 12:25:56 +0530

    config mongose and create schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    AuthModule, 
+    UserModule, 
+    MongooseModule.forRoot('process.env.MONGODB_URL' as string),
+    ConfigModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
